2837ec097893a057d71f76fc4b7c0dce3b7c7bf2
Vodkamishka <andrtomsk@yandex.com>
2019-05-21 18:33:22 +0700

    Updates

---
+++ precache-manifest.263f4a50e3af137631d086af8395810b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215121b5d675e580fb6cc6782352dc69",
+    "url": "/bicycles-app-store/index.html"
+  },
+  {
+    "revision": "e208e191fe144ca74117",
+    "url": "/bicycles-app-store/static/css/main.c0d09cf0.chunk.css"
+  },
+  {
+    "revision": "c2d0b7921f306d55154d",
+    "url": "/bicycles-app-store/static/js/2.0b3d3c7c.chunk.js"
+  },
+  {
+    "revision": "e208e191fe144ca74117",
+    "url": "/bicycles-app-store/static/js/main.3bf3fa35.chunk.js"
+  },
+  {
+    "revision": "7c8841d8fd0bb38744cd",
+    "url": "/bicycles-app-store/static/js/runtime~main.3463c26d.js"
+  }
+]);
